bd4bd28ed1b271859717bd9e8a5c281a9054396c
RJ <rjcodes.dev@gmail.com>
2023-09-07 20:50:52 -0400

    Create dask_bench.py

---
+++ dask_bench.py
@@ -0,0 +1,24 @@
+# Initialize Dask DataFrame
+df = dd.from_pandas(pd.DataFrame({'key': [f'key_{i}' for i in range(n)], 'value': [f'value_{i}' for i in range(n)]}), npartitions=5)
+
+start_time = time.time()
+for i in range(n):
+    new_row = dd.from_pandas(pd.DataFrame({'key': [f'key_{n + i}'], 'value': [f'value_{n + i}']}), npartitions=1)
+    df = df.append(new_row)
+end_time = time.time()
+print(f"Dask Insert Time: {end_time - start_time}")
+
+start_time = time.time()
+for _ in range(100):
+    key = f'key_{random.randint(0, n-1)}'
+    _ = df[df['key'] == key].compute()
+end_time = time.time()
+print(f"Dask Search Time: {end_time - start_time}")
+
+# Dask doesn't support in-place deletion, so we're just timing a filter operation here
+start_time = time.time()
+for _ in range(100):
+    key = f'key_{random.randint(0, n-1)}'
+    df = df[df['key'] != key]
+end_time = time.time()
+print(f"Dask Delete Time (via filtering): {end_time - start_time}")
