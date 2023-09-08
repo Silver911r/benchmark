4c2dfb8e069bdc149ab103320f075578091527ef
RJ <rjcodes.dev@gmail.com>
2023-09-07 20:59:35 -0400

    Update dask_bench.py
    fixed defining n and append

--- dask_bench.py
+++ dask_bench.py
@@ -5,12 +5,13 @@ import pandas as pd
 import dask.dataframe as dd
 
 # Initialize Dask DataFrame
+n = 1000  # Number of records
 df = dd.from_pandas(pd.DataFrame({'key': [f'key_{i}' for i in range(n)], 'value': [f'value_{i}' for i in range(n)]}), npartitions=5)
 
 start_time = time.time()
 for i in range(n):
     new_row = dd.from_pandas(pd.DataFrame({'key': [f'key_{n + i}'], 'value': [f'value_{n + i}']}), npartitions=1)
-    df = df.append(new_row)
+    df = dd.concat([df, new_row])
 end_time = time.time()
 print(f"Dask Insert Time: {end_time - start_time}")
 
