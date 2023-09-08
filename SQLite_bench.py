93ef0523b606ff775696ad51743c73ef47c00fa9
RJ <rjcodes.dev@gmail.com>
2023-09-07 20:51:23 -0400

    Create SQLite_bench.py

---
+++ SQLite_bench.py
@@ -0,0 +1,31 @@
+# Initialize SQLite database
+conn = sqlite3.connect(":memory:")
+cursor = conn.cursor()
+cursor.execute("CREATE TABLE table_db (key TEXT, value TEXT)")
+
+start_time = time.time()
+for i in range(n):
+    key = f'key_{i}'
+    value = f'value_{i}'
+    cursor.execute("INSERT INTO table_db VALUES (?, ?)", (key, value))
+conn.commit()
+end_time = time.time()
+print(f"SQLite Insert Time: {end_time - start_time}")
+
+start_time = time.time()
+for _ in range(100):
+    key = f'key_{random.randint(0, n-1)}'
+    cursor.execute("SELECT value FROM table_db WHERE key=?", (key,))
+    _ = cursor.fetchone()
+end_time = time.time()
+print(f"SQLite Search Time: {end_time - start_time}")
+
+start_time = time.time()
+for _ in range(100):
+    key = f'key_{random.randint(0, n-1)}'
+    cursor.execute("DELETE FROM table_db WHERE key=?", (key,))
+conn.commit()
+end_time = time.time()
+print(f"SQLite Delete Time: {end_time - start_time}")
+
+conn.close()
